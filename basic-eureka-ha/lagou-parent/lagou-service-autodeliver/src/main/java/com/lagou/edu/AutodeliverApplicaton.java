9be5cbbcc6fc38e9c089380c9d4c903f302ba3e3
RexChen <2363613998@qq.com>
2021-01-09 16:44:36 +0800

    创建Eureke HA项目

---
+++ basic-eureka-ha/lagou-parent/lagou-service-autodeliver/src/main/java/com/lagou/edu/AutodeliverApplicaton.java
@@ -0,0 +1,20 @@
+package com.lagou.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class AutodeliverApplicaton {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutodeliverApplicaton.class,args);
+    }
+
+    //使用Resttemple进行远程调用,先对注入该对象
+    @Bean
+    public RestTemplate getRestTemplete(){
+        return new RestTemplate();
+    }
+}
