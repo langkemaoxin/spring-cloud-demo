bcb98e2499eb012d578535c669d6cb922561c362
RexChen <2363613998@qq.com>
2021-01-09 21:56:27 +0800

    简历投递微服务注册Eureka

--- basic-eureka-ha-provider-consumer/lagou-parent/lagou-service-autodeliver/src/main/java/com/lagou/edu/AutodeliverApplicaton.java
+++ basic-eureka-ha-provider-consumer/lagou-parent/lagou-service-autodeliver/src/main/java/com/lagou/edu/AutodeliverApplicaton.java
@@ -2,10 +2,12 @@ package com.lagou.edu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient //开启注册中心客户端通用型
 public class AutodeliverApplicaton {
 
     public static void main(String[] args) {
