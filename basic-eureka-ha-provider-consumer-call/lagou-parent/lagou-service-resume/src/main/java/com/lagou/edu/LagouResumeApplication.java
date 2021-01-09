be358b43d9b1c9a83a1d50991c2b74f403a3e04d
RexChen <2363613998@qq.com>
2021-01-09 23:21:10 +0800

    服务调用示例

---
+++ basic-eureka-ha-provider-consumer-call/lagou-parent/lagou-service-resume/src/main/java/com/lagou/edu/LagouResumeApplication.java
@@ -0,0 +1,18 @@
+package com.lagou.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EntityScan("com.lagou.edu.pojo")
+//@EnableEurekaClient //Eureka独有 效果和EnableDiscoveryClient一样
+@EnableDiscoveryClient //开启注册中心客户端通用型
+//说明：从SpringCloud的 Edgware版本开始，不加注解也OK
+public class LagouResumeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LagouResumeApplication.class, args);
+    }
+}
