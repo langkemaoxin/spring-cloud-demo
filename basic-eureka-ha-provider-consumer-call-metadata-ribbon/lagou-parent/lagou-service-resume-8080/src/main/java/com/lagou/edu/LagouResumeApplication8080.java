51d1dbcb7a8c33b04f77cd849f59a4ab651b8758
RexChen <2363613998@qq.com>
2021-01-14 07:16:22 +0800

    Ribbon负载均衡

---
+++ basic-eureka-ha-provider-consumer-call-metadata-ribbon/lagou-parent/lagou-service-resume-8080/src/main/java/com/lagou/edu/LagouResumeApplication8080.java
@@ -0,0 +1,17 @@
+package com.lagou.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EntityScan("com.lagou.edu.pojo")
+//@EnableEurekaClient //Eureka独有 效果和EnableDiscoveryClient一样
+@EnableDiscoveryClient //开启注册中心客户端通用型
+//说明：从SpringCloud的 Edgware版本开始，不加注解也OK
+public class LagouResumeApplication8080 {
+    public static void main(String[] args) {
+        SpringApplication.run(LagouResumeApplication8080.class, args);
+    }
+}
