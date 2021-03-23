e98ae020e6e1ddaaf49e2eecad7f10352468ad8c
RexChen <2363613998@qq.com>
2021-03-24 06:28:44 +0800

    添加nacos

---
+++ Feign-GateWay-Config-Stream-Sleuth-OAuth2-Nacos/lagou-parent/lagou-service-resume-8082-nacos/src/main/java/com/lagou/edu/controller/ConfigController.java
@@ -0,0 +1,20 @@
+package com.lagou.edu.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/config")
+@RefreshScope
+public class ConfigController {
+    @Value("${abc.test}")
+    private String lagouMessage;
+
+    @GetMapping("/viewconfig")
+    public String viewConfig(){
+        return "message====> "+ lagouMessage;
+    }
+}
