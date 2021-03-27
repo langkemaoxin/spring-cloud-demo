f929168459e354cdbf40a13fb93631a11077bfba
RexChen <2363613998@qq.com>
2021-03-27 20:06:23 +0800

    add sentinel

--- Feign-GateWay-Config-Stream-Sleuth-OAuth2-Nacos-Sentinel/lagou-parent/lagou-service-autodeliver-8098-sentinel/src/main/java/com/lagou/edu/controller/AutoDeliverController.java
+++ Feign-GateWay-Config-Stream-Sleuth-OAuth2-Nacos-Sentinel/lagou-parent/lagou-service-autodeliver-8098-sentinel/src/main/java/com/lagou/edu/controller/AutoDeliverController.java
@@ -1,5 +1,7 @@
 package com.lagou.edu.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.lagou.edu.config.SentinelHandlersClass;
 import com.lagou.edu.service.ResumeServiceFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,8 +17,16 @@ public class AutoDeliverController {
     private ResumeServiceFeignClient client;
 
     @GetMapping("/checkState/{userId}")
+    @SentinelResource(value = "findResumeOpenState",
+            blockHandlerClass = SentinelHandlersClass.class, blockHandler = "handleException",
+            fallbackClass = SentinelHandlersClass.class,fallback = "handleError")
     public Integer findResumeOpenState(@PathVariable Long userId){
-
+//
+//        try {
+//            Thread.sleep(1000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
         return client.findResumeOpenState(userId);
     }
 
